cbc244ace7345d679651748e00becc076835a924
jattorri <jattorri@gmail.com>
2021-04-10 10:54:23 -0300

    quinto

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static('./dist/proyectoDelivery'));
+app.use(express.static('./proyectoDelivery'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/proyectoDelivery'}),
+    res.sendFile('index.html', {root: 'proyectoDelivery'}),
 );
 
 
