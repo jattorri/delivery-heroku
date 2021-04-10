f0c41eb5b004ccfdde584a369055c51a748da04a
jattorri <jattorri@gmail.com>
2021-04-10 10:22:24 -0300

    deploy heroku

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static('./dist/proyectoDelivery'));
+
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/proyectoDelivery'}),
+);
+
+
+app.listen(process.env.PORT || 8080);
+
+console.log(`Running on port ${process.env.PORT || 8080}`)
