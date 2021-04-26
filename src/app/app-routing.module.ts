51b9f9aa2eb1669578a932f0d6a928eb48e4d64c
jattorri <jattorri@gmail.com>
2021-04-26 13:12:05 -0300

    corregido3

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { AboutComponent } from './components/about/about.component';
 import { PlatosComponent } from './components/platos/platos.component';  
 import { DetallePlatoComponent } from './components/detalle-plato/detalle-plato.component';
 import { BuscadorComponent } from './components/buscador/buscador.component';
-import { TuPedidoComponent } from './components/tu-pedido/tu-pedido.component';
+
 import { EnviadoComponent } from './components/enviado/enviado.component';
 import { FormularioComponent } from './components/formulario/formulario.component';
 
@@ -15,7 +15,7 @@ const routes: Routes = [   { path: 'home', component: HomeComponent },
 { path: 'platos', component: PlatosComponent }, 
 { path: 'detallePlato/:id', component: DetallePlatoComponent},
 { path: 'buscar/:termino', component: BuscadorComponent },
-{ path: 'tu-pedido', component: TuPedidoComponent },
+
 { path: 'enviado', component: EnviadoComponent},
 { path: 'login', component:FormularioComponent},             
 { path: '**', pathMatch: 'full', redirectTo: 'home' }  
